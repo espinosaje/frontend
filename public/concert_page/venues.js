1213f8a7ff1d3798896cfcfb3fe36e0164b1b70c
espinosaje <espinosa.je@gmail.com>
2018-07-31 21:59:23 -0600

    Added ability to populate list
    Added ability to populate a dynamic list on the "blur" method of the
    input, so it will populate on loose focus.

--- public/concert_page/venues.js
+++ public/concert_page/venues.js
@@ -1,21 +1,30 @@
 $(document).ready(function() {
-	$.ajax({
+	//var $venues = $('#venues');
+
+	//populate the list when loosing focus
+	$("input").blur(function(){
+		$.ajax({
         url: "http://localhost:8081/maven_rest_ws-0.1.0/setlist/venue",		
-		data: { name: "garbage"}
-    }).then(function(data) {
-		alert("Hi: "+ data);
-       $('.name-id').append(data[0].name);   
-	   //$('.venue-id').append(data[0].id);
-    });
+		data: { name: $('#venue-in').val()}
+		}).then(function(data) {
+		   //$('.name-id').html(data[0].name);	
+			$.each(data,function(i,venue){
+				$('.venue-div').append('<li>name:'+venue.name+' </li>');
+			});
+		});
+	});
 	
+	//do something with the button, open a pop-up
 	$("button").click(function(){
 		$.ajax({
         url: "http://localhost:8081/maven_rest_ws-0.1.0/setlist/venue",		
 		data: { name: $('#venue-in').val()}
-    }).then(function(data) {
-       $('.name-id').append(data[0].name);	  
-    });
+		}).then(function(data) {
+		   alert("Do sone shit");		
+		});
 	});
+	
+	
 });
 	
 //$(document).on('click', '#venue-btn', function () {	
