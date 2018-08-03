24f71817ae20ec36eda1bf18d583e95e47e7977e
espinosaje <espinosa.je@gmail.com>
2018-08-02 20:28:46 -0600

    Added dynamic list and pop-up
    Added dynamic list and pop-up. Pop-up doesn't do anything yet.

--- public/concert_page/venues.js
+++ public/concert_page/venues.js
@@ -7,22 +7,39 @@ $(document).ready(function() {
         url: "http://localhost:8081/maven_rest_ws-0.1.0/setlist/venue",		
 		data: { name: $('#venue-in').val()}
 		}).then(function(data) {
-		   //$('.name-id').html(data[0].name);	
+		   //$('.name-id').html(data[0].name);
+			$('.venue-div li').remove();
 			$.each(data,function(i,venue){
-				$('.venue-div').append('<li>name:'+venue.name+' </li>');
+				//$('.venue-div').append('<li>name:'+venue.name+', city:'+venue.city.name+' </li>');
+				$('.venue-div').append('<li>- name:'+venue.name+'</li><li>city:'+venue.city.name+' </li>');
 			});
 		});
 	});
 	
 	//do something with the button, open a pop-up
 	$("button").click(function(){
+		//ToDo: Read the selected LI from the "venue-div" and send those as parameters for "/event/add"
+		var venue_params = $('#venue-in').val();
+
 		$.ajax({
         url: "http://localhost:8081/maven_rest_ws-0.1.0/setlist/venue",		
-		data: { name: $('#venue-in').val()}
+		data: { name: venue_params}
 		}).then(function(data) {
-		   alert("Do sone shit");		
+		   selectDialog('Pan','You had selected, Text: '+ $('#venue-in').val()+' And Value : '+data[0].name);	
 		});
 	});
+	
+	function selectDialog(title, text) {    
+            return $('<div></div>').append(text).dialog({
+                resizable: true,
+                modal: true,
+                buttons: {
+                    "OK": function() {
+                        $(this).dialog("close");
+                    }
+                }
+            });
+        }
 	
 	
 });
