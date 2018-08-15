52892ce406672fbe0843d3731061acf798bd9e02
espinosaje <espinosa.je@gmail.com>
2018-08-15 01:03:14 -0600

    Added Band and Date inputs

--- public/concert_page/venues.js
+++ public/concert_page/venues.js
@@ -4,6 +4,7 @@ $(document).ready(function() {
 	// if its "1", implement functionallity to look for the exact venue by pushing a button based on 3 fields
 	// if it's "0", show a list of venues matching the name and select the correct one
 	var venueSearch = 1;
+	var venue_data;
 	
 	if (venueSearch == 0){
 		$('#venue-location').hide();
@@ -20,7 +21,7 @@ $(document).ready(function() {
 				$('.venue-div li').remove();
 				var dynamic_list = '';
 				$.each(data,function(i,venue){
-					//$('.venue-div').append('<li>- name:'+venue.name+'</li><li>city:'+venue.city.name+' </li>');
+					$('.venue-div').append('<li>Venue Found:'+venue.name+'</li><li>, @'+venue.city.name+' </li>');
 					dynamic_list = dynamic_list + '<li>name:'+venue.name+' @ city: '+venue.city.name+' </li>';
 				});
 				
@@ -31,20 +32,80 @@ $(document).ready(function() {
 	});
 	
 	//look for specific vanue by name and location (alternative to showing list)
-	$("button").click(function(){
-		//ToDo: Read the selected LI from the "venue-div" and send those as parameters for "/event/add"
+	$("#venue-btn").click(function(){		
 		var p_venue = $('#venue-in').val();
 		var p_city = $('#city-in').val();
 		var p_state = $('#state-in').val();
-
+		
+		$('.venue-div li').remove();
+		
 		$.ajax({
+		statusCode: {
+		  500: function() {			
+			$('.venue-div').append('<li>Venue NOT Found </li>');
+		   }
+		},	
         url: "http://localhost:8081/maven_rest_ws-0.1.0/setlist/venue",		
 		data: { name: p_venue,
 				cityName: p_city,
 				stateCode: p_state}
 		}).then(function(data) {
-		   //selectDialog('Pan','You had selected, Text: '+ $('#venue-in').val()+' And Value : '+data[0].name);	
-		   alert ('result: '+data);
+			if (data != null){
+				$('.venue-div').append('<li>Venue Found:'+data[0].name+', @'+data[0].city.name+' </li>');
+				venue_data = data[0];
+			}
+		  
+		});
+	});
+	
+	//looks for an artists on blur, returns only the exact match
+	//ToDo: there could be 2 artist with the exact match, will need to select from a list if this is the case
+	$("#band-in").blur(function(){		
+		var band = $('#band-in').val();
+		//var p_city = $('#date-in').val();
+		
+		$('.band-div li').remove();
+		
+		$.ajax({
+		statusCode: {
+		  500: function() {			
+			$('.band-div').append('<li>Band NOT Found </li>');
+		   }
+		},	
+        url: "http://localhost:8081/maven_rest_ws-0.1.0/setlist/artist",		
+		data: { 
+			artistName: band
+		}
+		}).then(function(data) {
+			if (data != null){
+				$('.band-div').append('<li>Band Found:'+data.name+', id:'+data.mbid+' </li>');
+			}
+		  
+		});
+	});
+	
+	//add an EVENT with an artist (In progress - ToDo)
+	$("#event-btn").click(function(){		
+		var p_band = $('#band-in').val();
+		var p_date = $('#date-in').val();
+		
+		$('.band-div li').remove();
+		
+		$.ajax({
+		statusCode: {
+		  500: function() {			
+			$('.band-div').append('<li>Couldn''t create event</li>');
+		   }
+		},	
+        url: "http://localhost:8081/maven_rest_ws-0.1.0/event/add",		//?name=Tool&venue=3&tour=Lateralus&year=2001&fest=0&date=31-10-2001
+		data: { name: p_band,
+				venue: venue_data.id,
+				tour: p_state}
+		}).then(function(data) {
+			if (data != null){
+				$('.band-div').append('<li>Band Found:'+data[0].name+', @'+data[0].city.name+' </li>');
+			}
+		  
 		});
 	});
 	
